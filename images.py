1610aa6c1c3862599c4e3949b727ca61d2189dd8
Artem <spb9719268@yandex.ru>
2023-12-08 09:28:00 +0300

    заработала функция преобразования рисунка на монохром(до этого не работала из функции)

--- images.py
+++ images.py
@@ -13,7 +13,7 @@ pag.typewrite('o', 0)
 time.sleep(1)
 sshot = pag.screenshot()
 time.sleep(0.2)
-image_location = pag.locateOnScreen('51.jpg')
+image_location = pag.locateOnScreen('change_pers_button.jpg', grayscale=True)
 print(image_location)
 # if image_location is not None:
 #     print("Image found at ({}, {})".format(image_location.left, image_location.top))
